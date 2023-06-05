c707407e0a484d2255cbd846b66f3f4fbd325bce
Adele <bidemidone@gmail.com>
2023-06-05 16:04:54 -0700

    updated the check_cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,15 +10,16 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	if(!list)
+	if (!list)
 		return (0);
 
-	while(slow && fast && fast->next)
+	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
-		if(slow == fast)
+		if (slow == fast)
 			return (1);
 	}
+
 	return (0);
 }

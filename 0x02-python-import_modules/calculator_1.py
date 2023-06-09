67b90800bebf06dbf0fa9fd2b2821a43c091467c
Adele <bidemidone@gmail.com>
2023-06-08 20:42:10 -0700

    updated the python import module files

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addirtion function
+
+    args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    the return value a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtractiion function
+
+    args:
+    a: first integer
+    b: second integer
+
+    returns:
+    the return value a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    args:
+    a: first integer
+    b: second integer
+
+    returns:
+    the reurn value a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    args:
+    a: first integer
+    b: second integer
+
+    returns:
+    the return value a/b
+    """
+
+    return int(a/b)
